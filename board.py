749726c0a06424d6f020b59025c22ebf4f2a1647
rowida117 <rowida.mohamed04@eng-st.cu.edu.eg>
2025-06-14 14:58:54 +0300

    Update board.py

--- board.py
+++ board.py
@@ -73,10 +73,10 @@ class Board:
 
 """
 check win algorithm 
-for vertical check for each (r,c ) we check (r,c) , (r+1 ,c ) , ( r+2 ,c ) , (r+3 ,c )
-for horizontal check for each (r,c ) we check  (r,c ), (r , c+1),(r ,c+2 ), (r,c+3 )
-for diagonal/ check  for each (r,c ) we check  (r, c),(r-1, c+1),(r-2, c+2),(r-3, c+3)
-for diagonal \ check for each  (r,c ) we check (r, c),(r+1, c+1),(r+2, c+2),(r+3, c+3)
+for vertical check for each (r,c ) we check (r,c) , (r+1 ,c ) , ( r+2 ,c ) , (r+3 ,c )    Loops over 7 cols × 4 rows = 28 checks
+for horizontal check for each (r,c ) we check  (r,c ), (r , c+1),(r ,c+2 ), (r,c+3 )  Loops over 7 rows × 4 cols = 28 checks
+for diagonal/ check  for each (r,c ) we check  (r, c),(r-1, c+1),(r-2, c+2),(r-3, c+3)  4 rows × 4 cols = 16 checks
+for diagonal \ check for each  (r,c ) we check (r, c),(r+1, c+1),(r+2, c+2),(r+3, c+3)  4 rows × 4 cols = 16 checks
 
 """
     def play_game():
